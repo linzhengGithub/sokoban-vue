f877fdb0957722c6d44b00ce7bb6d0f09418be9a
linzheng <alidlg9045@126.com>
2023-11-09 23:51:22 +0800

    二维数组初步实现地图

--- store/map.ts
+++ store/map.ts
@@ -1,6 +1,17 @@
 import { defineStore } from "pinia";
 
+export enum MapTile {
+  WALL = 1,
+  FLOOR = 2
+}
+
 export const useMapStore = defineStore('mapStore', () => {
-  const map = [1, 2, 3]
+  const map = [
+    [1, 1, 1, 1, 1],
+    [1, 2, 2, 2, 1],
+    [1, 2, 2, 2, 1],
+    [1, 2, 2, 2, 1],
+    [1, 1, 1, 1, 1]
+  ]
   return { map }
 })
